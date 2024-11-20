e93db40d22883b48c01608f35a71ce012b1fd00c
Maundytime <maundy.time@gmail.com>
2024-11-20 09:55:58 +0800

    Add framework

---
+++ ClockEffect.xcframework/ios-arm64/ClockEffect.framework/Headers/ClockEffect.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClockEffect.
+FOUNDATION_EXPORT double ClockEffectVersionNumber;
+
+//! Project version string for ClockEffect.
+FOUNDATION_EXPORT const unsigned char ClockEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClockEffect/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -5,17 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "ClockEffect",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ClockEffect",
             targets: ["ClockEffect"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ClockEffect"),
+        .binaryTarget(
+            name: "ClockEffect",
+            path: "./ClockEffect.xcframework"
+        ),
 
     ]
 )
